182f8399e25b2a19c9ef1116e58a0aaa3c19eff7
Mike Mulhausen <Mike.Mulhausen@coyote.com>
2019-06-13 23:38:19 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,73 @@
+/**
+ * Copyright (c) 2015-present, Viro Media, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+'use strict';
+
+import React, { Component } from 'react';
+
+import {
+  AppRegistry,
+} from 'react-native';
+
+import {
+  ViroVRSceneNavigator,
+  ViroARSceneNavigator,
+} from 'react-viro';
+
+var createReactClass = require('create-react-class');
+
+/*
+ * TODO: Add your API key below!!
+ */
+var apiKey = "37EB63B6-8181-49C8-9B71-518C7D6E0904";
+
+var vrScenes = {
+    '360PhotoTour': require('./js/360PhotoTour/MainScene'),
+    'HumanBody': require('./js/HumanBody/MainScene'),
+    'ProductShowcase': require('./js/ProductShowcase/ProductShowcase'),
+    'ViroMediaPlayer': require('./js/ViroMediaPlayer/ViroTheatre'),
+    'ParticleEmitters': require('./js/ParticleEmitters/ViroParticleTemplates'),
+    'PhysicsSample': require('./js/PhysicsSample/BasicPhysicsSample'),
+}
+
+var arScenes = {
+  'ARSimpleSample': require('./js/ARSample/HelloWorldSceneAR.js'),
+  'ARPhysicsSample': require('./js/ARPhysicsSample/BasicPhysicsSample.js'),
+  'ARCarDemo' : require('./js/ARCarDemo/ARCarDemo.js'),
+  'ARPosterDemo' : require('./js/ARPosterDemo/ARPosterDemo.js'),
+  'BusinessCard' : require('./js/ARBusinessCard/BusinessCard.js'),
+}
+
+var showARScene = true;
+
+var ViroCodeSamplesSceneNavigator = createReactClass({
+  render: function() {
+
+    if (showARScene) {
+      return (
+        <ViroARSceneNavigator
+          initialScene={{
+            scene: arScenes['ARCarDemo'],
+          }}
+          apiKey={apiKey} />
+        );
+    } else {
+      return (
+        <ViroVRSceneNavigator
+          initialScene={{
+            scene: vrScenes['360PhotoTour'],
+          }}
+          apiKey={apiKey} />
+      );
+
+    }
+  }
+});
+
+module.exports = ViroCodeSamplesSceneNavigator;
